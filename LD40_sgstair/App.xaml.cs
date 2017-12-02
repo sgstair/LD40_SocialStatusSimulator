72c09b77ea02371944edf23d88ac86407addad42
Stephen Stair <sgstair@akkit.org>
2017-12-02 13:16:00 -0800

    Add project files.

---
+++ LD40_sgstair/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LD40_sgstair
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
